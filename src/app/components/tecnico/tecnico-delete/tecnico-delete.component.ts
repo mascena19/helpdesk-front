f8d5144db042437ff613a67750a9e3a9dc72d640
Rodrigo Mascena <mascena19@gmail.com>
2024-11-29 21:38:25 -0300

    TECNO DELETE COMPLETO

---
+++ src/app/components/tecnico/tecnico-delete/tecnico-delete.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Tecnico } from 'src/app/models/tecnico';
+import { TecnicoService } from 'src/app/services/tecnico.service';
+
+@Component({
+  selector: 'app-tecnico-delete',
+  templateUrl: './tecnico-delete.component.html',
+  styleUrls: ['./tecnico-delete.component.css']
+})
+export class TecnicoDeleteComponent implements OnInit {
+
+  tecnico: Tecnico = {
+    id:         '',
+    nome:       '',
+    cpf:        '',
+    email:      '',
+    senha:      '',
+    perfis:     [],
+    dataCriacao: ''
+ }
+
+ 
+ constructor(
+    private service: TecnicoService,
+    private toast: ToastrService,
+    private router: Router,
+    private route: ActivatedRoute,
+ ) { }
+
+ ngOnInit(): void { 
+    this.tecnico.id = this.route.snapshot.paramMap.get('id');
+    this.findById();
+ }
+
+ findById(): void {
+   this.service.findById(this.tecnico.id).subscribe(resposta => {
+      resposta.perfis = [];
+      this.tecnico = resposta;
+   });
+ }
+ 
+ delete(): void {
+  this.service.delete(this.tecnico.id).subscribe(
+    () => {
+      this.toast.success('Técnico deletado com sucesso', 'Delete');
+      this.router.navigate(['tecnicos'])
+    },
+    ex => {
+      console.log(ex);
+
+      if (ex.error.errors) {
+        ex.error.errors.forEach((element: any) => {
+          this.toast.error(element.message);
+        });
+      } else {
+        const errorMessage = ex.error.message;
+        if (errorMessage.includes("interpolatedMessage")) {
+          const interpolatedMessage = errorMessage.split("interpolatedMessage='")[1].split("',")[0];
+          this.toast.error(interpolatedMessage);
+        } else {
+          this.toast.error(errorMessage);
+        }
+      }
+    }
+  );
+}
+ 
+}
